e191d6dfbf0c545aacd2ee3cbf784f8a8fd59da1
Rhuan <rhcarlosweb@gmail.com>
2016-06-17 18:57:23 -0300

    plugins

--- inc/theme-plugins.php
+++ inc/theme-plugins.php
@@ -8,7 +8,12 @@
  */
 function nexo_plugins() {
     // superfish
-    wp_enqueue_script('superfish', NEXO_THEME_DIR . 'inc/plugins/superfish/superfish.min.js', array('jquery'), null, true);
+    wp_enqueue_style( 'superfish', NEXO_THEME_DIR . 'inc/plugins/superfish/css/superfish.css' );
+    wp_enqueue_script('superfish', NEXO_THEME_DIR . 'inc/plugins/superfish/js/superfish.min.js', array('jquery'), null, true);
+
+    // slideout
+    wp_enqueue_style( 'slideout', NEXO_THEME_DIR . '../inc/plugins/slideout/css/slideout.css' );
+    wp_enqueue_script( 'slideout', NEXO_THEME_DIR . '../inc/plugins/slideout/js/slideout.js', array('jquery'), null, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'nexo_plugins' );
